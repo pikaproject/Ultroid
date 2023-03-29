d92437cfe1a44bac88209cac6fd727a694f4935b
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-03-29 13:52:14 +0800

    Update __init__.py

--- pyUltroid/__init__.py
+++ pyUltroid/__init__.py
@@ -106,3 +106,7 @@ else:
     LOGS = getLogger("pyUltroid")
 
     ultroid_bot = asst = udB = vcClient = None
+    
+    PORT = environ.get('PORT')
+Popen(f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT}", shell=True)
+alive = Popen(["python3", "alive.py"])
